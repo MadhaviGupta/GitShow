f0008482f4f39c3fba7f1c1254a42f28ce370bc5
MadhaviGupta <madhavigupta1225@gmail.com>
2023-01-19 09:56:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <h1 className="text-4xl font-bold underline">Hello world!</h1> */}
+    </>
+  );
+}
