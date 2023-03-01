369f6325625b40f8ca566ac32649d82902f8c3c0
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-03-01 09:56:19 +0530

    Notification Bug Fixed

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -5,35 +5,6 @@ import { FaRegCommentDots, FaHeart } from "react-icons/fa";
 
 const cookie = document.cookie;
 
-function NotificationCard(props) {
-  return (
-    props.username === cookie && (
-      <div className="">
-        {props.likedBy.map((user) => {
-          return (
-            user !== props.username && (
-              <p className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center">
-                <FaHeart className="text-red-500 mr-5" />
-                {user} has liked your post
-              </p>
-            )
-          );
-        })}
-        {props.commentObj.map((user) => {
-          return (
-            user.username !== props.username && (
-              <p className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center">
-                <FaRegCommentDots className="text-blue-400 mr-5" />
-                {user.username} has commented on your post
-              </p>
-            )
-          );
-        })}
-      </div>
-    )
-  );
-}
-
 export default function Notifications() {
   const [post, setPost] = useState([]);
   useEffect(() => {
@@ -71,5 +42,41 @@ export default function Notifications() {
         </div>
       </div>
     </div>
+  );
+}
+
+function NotificationCard(props) {
+  let i = 0;
+  return (
+    props.username === cookie && (
+      <div className="">
+        {props.likedBy.map((user) => {
+          return (
+            user !== props.username && (
+              <p
+                key={i++}
+                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
+              >
+                <FaHeart className="text-red-500 mr-5" />
+                {user} has liked your post
+              </p>
+            )
+          );
+        })}
+        {props.commentObj.map((user) => {
+          return (
+            user.username !== props.username && (
+              <p
+                key={i++}
+                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
+              >
+                <FaRegCommentDots className="text-blue-400 mr-5" />
+                {user.username} has commented on your post
+              </p>
+            )
+          );
+        })}
+      </div>
+    )
   );
 }
