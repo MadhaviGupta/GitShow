1df2ee944d254c5e68772c74bc4d710d69f8996b
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-03-01 10:11:58 +0530

    Minor Fix

---
+++ src/components/NotificationCard.js
@@ -0,0 +1,37 @@
+import { FaRegCommentDots, FaHeart } from "react-icons/fa";
+export default function NotificationCard(props) {
+  const cookie = document.cookie;
+  let i = 0;
+  return (
+    props.username === cookie && (
+      <div className="">
+        {props.likedBy.map((user) => {
+          return (
+            user !== props.username && (
+              <p
+                key={i++}
+                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
+              >
+                <FaHeart className="text-red-500 mr-5" />
+                {user} has liked your post
+              </p>
+            )
+          );
+        })}
+        {props.commentObj.map((user) => {
+          return (
+            user.username !== props.username && (
+              <p
+                key={i++}
+                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
+              >
+                <FaRegCommentDots className="text-blue-400 mr-5" />
+                {user.username} has commented on your post
+              </p>
+            )
+          );
+        })}
+      </div>
+    )
+  );
+}
--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import db from "../firebase.config";
-import { FaRegCommentDots, FaHeart } from "react-icons/fa";
-
-const cookie = document.cookie;
+import NotificationCard from "../components/NotificationCard";
 
 export default function Notifications() {
   const [post, setPost] = useState([]);
@@ -42,41 +40,5 @@ export default function Notifications() {
         </div>
       </div>
     </div>
-  );
-}
-
-function NotificationCard(props) {
-  let i = 0;
-  return (
-    props.username === cookie && (
-      <div className="">
-        {props.likedBy.map((user) => {
-          return (
-            user !== props.username && (
-              <p
-                key={i++}
-                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
-              >
-                <FaHeart className="text-red-500 mr-5" />
-                {user} has liked your post
-              </p>
-            )
-          );
-        })}
-        {props.commentObj.map((user) => {
-          return (
-            user.username !== props.username && (
-              <p
-                key={i++}
-                className="p-4 bg-black bg-opacity-40 my-1 rounded-lg flex items-center"
-              >
-                <FaRegCommentDots className="text-blue-400 mr-5" />
-                {user.username} has commented on your post
-              </p>
-            )
-          );
-        })}
-      </div>
-    )
   );
 }
